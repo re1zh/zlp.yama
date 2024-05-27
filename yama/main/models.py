f3f179dcb177b69dac9caaf74c6ad39abb0ca029
Daniil Samoilov <dansamoru@ya.ru>
2024-05-27 11:30:29 +0300

    Add user home page

--- yama/main/models.py
+++ yama/main/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Debt(models.Model):
-    creditor = models.CharField(max_length=100)
-    debtor = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    creditor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='debts')
+    debtor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='loans')
+    is_confirmed = models.BooleanField(default=False)
+    amount = models.DecimalField(max_digits=16, decimal_places=2)
     description = models.TextField(blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    date_due = models.DateTimeField()
+    date_due = models.DateField()
 
     def __str__(self):
         return f'{self.debtor} owes {self.amount} to {self.creditor}'
